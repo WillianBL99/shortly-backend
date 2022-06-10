c9d0c75d761b878d9655bbce4d28201908a82a4a
paulo uilian <uilianlbarros@gmail.com>
2022-06-09 21:56:46 -0300

    mend

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import routes from './routes/routes.js';
 dotenv.config();
 
 const port = process.env.PORT || 4005;
+console.log('portas', port)
 const app = express();
 app.use(cors());
 app.use(json());
