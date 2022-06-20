fd7680106fc3c57fc1ed9a5921e341a30d96da66
Guga <contato.gustavobarbosa@outlook.com>
2022-06-20 15:45:50 -0300

    Preparando revisão de código

--- src/utils/messageError.js
+++
@@ -1,6 +0,0 @@
-export default function messageError(message, error, res){
-    console.log(`${message}:`, error);
-    res.status(500).send(
-      { error: `Internal server. ${message}` }
-    );
-}
